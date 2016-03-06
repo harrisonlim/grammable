53e217a683acec548f9a7be99041361f59e6c8a5
harrisonlim <harrison.lim.hx@gmail.com>
2016-03-06 16:03:28 +0000

    added migration to connect gram to user, wrote tests for user authentication

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,14 +30,15 @@ end
 ActiveRecord::Migration.check_pending!
 
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -52,7 +53,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-  
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
