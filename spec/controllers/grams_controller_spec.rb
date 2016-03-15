aad2be133fa984f0ac44acc823b6b74ddb0809a9
harrisonlim <harrison.lim.hx@gmail.com>
2016-03-15 08:02:44 +0000

    wrote passing tests for grams#update action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -3,12 +3,22 @@ require 'rails_helper'
 RSpec.describe GramsController, type: :controller do
   describe "grams#update" do
     it "should allow users to successfully update grams" do
+      q = FactoryGirl.create(:gram, message: 'Initial Value')
+      patch :update, id: q.id, gram: { message: 'Changed' }
+      expect(response).to redirect_to root_path
+      q.reload
+      expect(q.message).to eq 'Changed'
     end
 
     it "should have http 404 error if the gram cannot be found" do
+      patch :update, id: "YOLOSWAG", gram: { message: 'Changed' }
+      expect(response).to have_http_status(:not_found)
     end
 
     it "should render the edit form with an http status of unprocessable_entity" do
+      q = FactoryGirl.create(:gram, message: 'Initial Value')
+      patch :update, id: q.id, gram: { message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 
